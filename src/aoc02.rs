13d3e0c30ae8ffbd21ea47763a2a3e8262f805a2
Henk van der Laan <opensource@henkvdlaan.com>
2020-12-04 21:39:57 +0100

    Day 02: Rewrite in linear style
    This works much faster than the iterator/state machine
    
    Before: 62.156 us 62.218 us 62.284 us
    After: 12.366 us 12.377 us 12.390 us

--- src/aoc02.rs
+++ src/aoc02.rs
@@ -1,67 +1,44 @@
 pub(crate) fn run(b: &[u8]) -> String {
+    let len = b.len();
+    let mut p = 0;
     let mut good = 0;
     let mut good2 = 0;
-    let mut cnt = 0;
-    let mut cnt2 = 0;
-    let mut min = 0;
-    let mut max = 0;
-    let mut chr = '!' as u8;
-    let mut st: i8 = -4;
-    b.iter().for_each(|v| {
-        let b = *v;
-        //print!("{}", b as char);
-        match st {
-            -4 => {
-                if b == '-' as u8 {
-                    st = -3;
-                } else if b == '\n' as u8 {
-                    return;
-                } else {
-                    min *= 10;
-                    min += b - '0' as u8;
-                }
-            }
-            -3 => {
-                if b == ' ' as u8 {
-                    st = -2;
-                } else {
-                    max *= 10;
-                    max += b - '0' as u8;
-                }
-            }
-            -2 => {
-                chr = b;
-                st = -1;
-            }
-            -1 => st += 1,
-            0 => st += 1,
-            n => {
-                if b == '\n' as u8 {
-                    st = -4;
-                    if cnt >= min && cnt <= max {
-                        good += 1;
-                    }
-                    if cnt2 == 1 {
-                        good2 += 1;
-                        //println!(" valid");
-                    }
-                    min = 0;
-                    max = 0;
-                    cnt = 0;
-                    cnt2 = 0;
-                } else {
-                    if b == chr {
-                        //println!("{} {}", n, b);
-                        cnt += 1;
-                        let p = (n) as u8;
-                        if p == min || p == max {
-                            cnt2 += 1;
-                        }
-                    }
-                    st += 1;
-                }
-            }
+    while p < len {
+        let mut min = b[p] - '0' as u8;
+        p += 1; // possible second number or dash
+        while b[p] != '-' as u8 {
+            min *= 10;
+            min += b[p] - '0' as u8;
+            p += 1;
         }
-    });
+        p += 1; //move over the dash
+        let mut max = b[p] - '0' as u8;
+        p += 1; // possible second number or space
+        while b[p] != ' ' as u8 {
+            max *= 10;
+            max += b[p] - '0' as u8;
+            p += 1;
+        }
+        p += 1; //move over the space
+        let c = b[p];
+        p += 2; // move over char and : to stand on space
+                // part 2
+        if (b[p + min as usize] == c) ^ (b[p + max as usize] == c) {
+            good2 += 1
+        }
+        p += 1; //move over the space
+        let mut cnt = 0;
+        while b[p] != '\n' as u8 {
+            if b[p] == c {
+                cnt += 1;
+            }
+            p += 1
+        }
+        p += 1;
+        if cnt >= min && cnt <= max {
+            good += 1;
+        }
+    }
+
     format!("{} {}\n", good, good2)
 }
