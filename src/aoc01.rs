e4b099a3f91c36a85d082f1017a1ce5b5bb5d821
Henk van der Laan <opensource@henkvdlaan.com>
2020-12-04 20:48:57 +0100

    Day 01: Move p1 calc to own loop, shortcut p2
    Minor impact,
    
    Before: 4.5635 us 4.5739 us 4.5860 us
    After: 4.3584 us 4.3668 us 4.3761 us

--- src/aoc01.rs
+++ src/aoc01.rs
@@ -3,7 +3,7 @@ use bit_set::BitSet;
 pub(crate) fn run(data: &[u8]) -> String {
     let mut singles = BitSet::with_capacity(2020);
     let mut acc: usize = 0;
-    let mut p1 = 0;
+    //let mut p1 = 0;
     let mut p2 = 0;
     data.iter().for_each(|i| {
         let x = *i as usize;
@@ -11,17 +11,27 @@ pub(crate) fn run(data: &[u8]) -> String {
             acc *= 10;
             acc += x - '0' as usize;
         } else {
-            if singles.contains(2020 - acc) {
-                //println!("Part 1: {}", acc as i32 * (2020 - acc as i32));
-                p1 = acc as i32 * (2020 - acc as i32);
-                //exit(0);
-            }
+            // if singles.contains(2020 - acc) {
+            //println!("Part 1: {}", acc as i32 * (2020 - acc as i32));
+            //     p1 = acc as i32 * (2020 - acc as i32);
+            //exit(0);
+            //  }
             if acc != 0 {
                 singles.insert(acc);
             }
             acc = 0;
         }
     });
+    let p1 = singles
+        .iter()
+        .find_map(|x| {
+            if singles.contains(2020 - x) {
+                Some(x * (2020 - x))
+            } else {
+                None
+            }
+        })
+        .unwrap();
     let mut xi = singles.iter();
     'p2: loop {
         match xi.next() {
@@ -37,6 +47,9 @@ pub(crate) fn run(data: &[u8]) -> String {
                                 //println!("Part 2: {}", *x as i32 * *y as i32 * z as i32);
                                 p2 = x * y * z;
                                 break 'p2;
+                            } else if x + y > 2020 {
+                                //bitvec is sorted
+                                break;
                             }
                         }
                     }
